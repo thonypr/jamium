0f24e8a7a76f0f9e0128e9e819cd9fda3602f99e
thony <antonprimako@gmail.com>
2019-11-07 01:16:02 +0300

    task 4

--- src/main/java/Responses.java
+++ src/main/java/Responses.java
@@ -18,7 +18,7 @@ public final class Responses {
     public static final String TASK_4_2 = "Формат ответа: строка в 5 символов (цифры и английские буквы; регистр не имеет значения)";
     public static final String CONGRAT_4_1 = "Верно!\n Верно";
     public static final String CONGRAT_4_2 = "Верно!";
-    public static final String CLOSE_4_1 = "На верном пути!";
+    public static final String CLOSE_4_1 = "Фёдор Двинятин!\nФЁДОР ДВИНЯТИН!\nЭто я не вам! :)";
     public static final String CLOSE_4_2 = "На верном пути!";
 
     private Responses() { }
