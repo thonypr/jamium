06dace4076a8222f24830b9b3e123b84fa786bb2
thony <antonprimako@gmail.com>
2019-11-11 00:47:56 +0300

    fix task 4

--- src/main/java/Responses.java
+++ src/main/java/Responses.java
@@ -7,12 +7,12 @@ public final class Responses {
             "а это именно 1000003";
     public static final String CLOSE_1 = "Почти! Ещё немного и будет верный ответ!";
     public static final String TASK_2 = "Формат ответа: число";
-    public static final String TASK_3_1 = "Формат ответа: число (13 символов)";
-    public static final String TASK_3_2 = "Формат ответа: строка";
-    public static final String CONGRAT_2 = "Верно!\n https://www.youtube.com/watch?v=G2_Q9FoD-oQ";
-    public static final String CONGRAT_3_1 = "Верно!\n Верно";
-    public static final String CONGRAT_3_2 = "Верно!";
     public static final String CLOSE_2 = "На верном пути!";
+    public static final String CONGRAT_2 = "Верно!\n https://www.youtube.com/watch?v=G2_Q9FoD-oQ";
+    public static final String TASK_3_1 = "Формат ответа: число (13 цифр)";
+    public static final String TASK_3_2 = "Формат ответа: строка";
+    public static final String CONGRAT_3_1 = "Верно!\n Верно";
+    public static final String CONGRAT_3_2 = "Верно!\nПотому что главное сокровище - это ты!";
     public static final String CLOSE_3_1 = "На верном пути!";
     public static final String CLOSE_3_2 = "На верном пути!";
     public static final String TASK_4_1 = "Формат ответа: слово (регистр не имеет значения, 10 букв)";
