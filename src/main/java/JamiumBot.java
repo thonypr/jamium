c03609f340fb127574ad4f10eb2cd449f3b31b3c
thony <antonprimako@gmail.com>
2019-12-20 00:44:38 +0300

    task #5

--- src/main/java/JamiumBot.java
+++ src/main/java/JamiumBot.java
@@ -604,7 +604,7 @@ public class JamiumBot extends TelegramLongPollingBot {
                             UsersController.updateUserState(chat_id, State.VIEW_TASK_5_3);
                             DBConnection.updateUser(chat_id, State.VIEW_TASK_5_3);
 //                            message.setReplyMarkup(InlineKeyboardResponses.getTasksKeyboard());
-                            message.setPhoto("AgADAgADw6sxG9hMYUuJjrrVtcbgEkp5wQ8ABAEAAwIAA20AA993AQABFgQ");
+                            message.setPhoto("AgADAgADa60xG7X54EswPOyyiINPmz6Wwg8ABAEAAwIAA20AA-n5AQABFgQ");
                             message.setChatId(chat_id);
                             message.setCaption(String.format("%s \n\n%s", response, Responses.TASK_5_3_TEXT));
                             try {
@@ -938,7 +938,7 @@ public class JamiumBot extends TelegramLongPollingBot {
                 UsersController.updateUserState(chat_id, State.VIEW_TASK_5_3);
                 DBConnection.updateUser(chat_id, State.VIEW_TASK_5_3);
 //                            message.setReplyMarkup(InlineKeyboardResponses.getTasksKeyboard());
-                message.setPhoto("AgADAgADw6sxG9hMYUuJjrrVtcbgEkp5wQ8ABAEAAwIAA20AA993AQABFgQ");
+                message.setPhoto("AgADAgADa60xG7X54EswPOyyiINPmz6Wwg8ABAEAAwIAA20AA-n5AQABFgQ");
                 message.setChatId(chat_id);
                 message.setCaption(String.format("%s \n\n%s", Responses.TASK_5_3_TEXT, Responses.TASK_5_3));
                 try {
